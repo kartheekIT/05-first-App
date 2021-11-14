bf4793502e6af90317eeb28d5d6f3963d7e493fa
kartheek <kartheek@plasticontech.com>
2021-11-14 18:35:40 +0530

    first message

--- src/main/java/com/example/springboot/Springrestapi1Application.java
+++ src/main/java/com/example/springboot/Springrestapi1Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springrestapi1Application {
-
+	int a=10;
+	int b=20;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Springrestapi1Application.class, args);
 	}
